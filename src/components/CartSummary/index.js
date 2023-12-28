89f312afacd3d1906d57dc3879f9cb28acf7819d
Farhanalisayed <s.farhan.ali.@gmail.com>
2023-12-28 19:56:25 +0000

    solution

---
+++ src/components/CartSummary/index.js
@@ -0,0 +1,22 @@
+// Write your code here
+import CartContext from '../../context/CartContext'
+
+const CartSummary = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      const amountList = cartList.map(each => each.price)
+      const amount = amountList.reduce((acc, addOn) => acc + addOn)
+      return (
+        <div>
+          <h1>
+            Order Total: <span>{amount}/-</span>
+          </h1>
+          <p>{cartList.length} Items in cart</p>
+          <button>Checkout</button>
+        </div>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default CartSummary
